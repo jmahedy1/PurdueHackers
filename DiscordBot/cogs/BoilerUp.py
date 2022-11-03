6905a22f4ee235df0ab870ece99f4b986b7cdc93
Jack Mahedy <jmahedy@purdue.edu>
2022-11-03 03:46:05 +0000

    Created Bot

---
+++ DiscordBot/cogs/BoilerUp.py
@@ -0,0 +1,15 @@
+import discord
+from discord.ext import commands
+from discord import app_commands
+
+class BoilerUp(commands.Cog):
+
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+
+    @app_commands.command(name= 'boiler', description = 'BOILER UP!')
+    async def boiler(self, interaction: discord.Interaction, text: str) -> None:
+        await interaction.response.send_message(f"{text}!", ephemeral=True)
+
+async def setup(bot: commands.Bot):
+    await bot.add_cog(BoilerUp(bot))
